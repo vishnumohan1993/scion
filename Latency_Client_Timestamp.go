ba8a9a1f57303ece5e155c89b84ad6e6566a24f5
vishnumohan1993 <38094703+vishnumohan1993@users.noreply.github.com>
2019-01-19 20:53:20 +0100

    Add files via upload

---
+++ Latency_Client_Timestamp.go
@@ -0,0 +1,143 @@
+// Example reference used from https://github.com/netsec-ethz/scion-homeworks/tree/master/latency
+
+package main
+
+import (
+	"flag"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math/rand"  //for mathematical computations
+	"time"  // estimating latency time stamps
+
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/sciond"
+)
+
+
+
+
+func check(f error) {
+	if f != nil {
+		log.Fatal(f)
+	}
+}
+
+const (
+	count = 15 // iteration count
+	limit = 30// maximum allowed of above count
+)
+
+
+func printUsage() {
+	fmt.Println("\ntimestamp_client -c ClientSCIONAddress -s ServerSCIONAddress")
+	fmt.Println("\tThe SCION address is specified as ISD-AS,[IP Address]:Port")
+}
+
+
+func main() {
+
+	var (
+		addressclient string  
+	    addressserver string
+
+		err    error
+		local  *snet.Addr
+		remote *snet.Addr
+
+		udpConnection *snet.Conn
+	)
+
+	flag.StringVar(&addressclient, "c", "", "Client SCION Address")  // binding the flag to client address string
+	flag.StringVar(&addressserver, "s", "", "Server SCION Address") // binding the flag to server address string
+	flag.Parse() // parsing command line to above defined flags
+
+
+//Creating Scion UDP socket
+	if  len(addressclient)>0           //  statement refers to equatting  length of clientaddress with a condition
+	{
+		local, err = snet.AddrFromString(addressclient)
+		 check(err)
+	}
+
+	//if len(addressclient) > 0 {           
+	//	local, err = snet.AddrFromString(addressclient)
+		//check(err) //passing the error to check function for logging it
+
+		//} 
+		//else
+		//{
+		//printUsage()
+		//check(fmt.Errorf("Error, client address needs to start with -c"))
+	//}
+
+	if  len(addressserver) > 0 {           //  statement refers to equatting  length of serveraddress with a condition
+		local, err = snet.AddrFromString(addressserver)
+		check(err)
+	}
+
+	//if len(addressserver) > 0 { // same as above
+		//remote, err = snet.AddrFromString(addressserver)   //Adding server address for establishing connection
+		//check(err)
+	//} else {
+		//printUsage()
+		//check(fmt.Errorf("Error, server address needs to be specified with -s"))
+	//}
+
+dispatcherAddr := "/run/shm/dispatcher/default.sock"
+snet.Init(local.IA, sciond.GetDefaultSCIONDPath(nil), dispatcherAddr)
+
+udpConnection, err = snet.DialSCION("udp4", local, remote)
+	check(err)
+
+	bufferreceivePacket := make([]byte, 5000) //packet buffer array of size 5000 made for receiving
+	buffersendPacket := make([]byte, 25) //packet buffer array of size 30 made for sending
+
+	seed := rand.NewSource(time.Now().UnixNano())  //creating seed with random ids from new source
+
+
+	//calculation starts here
+
+	var ans int64 = 0
+
+	i := 0 //iterations
+	j := 0 //limit
+
+	for i < count && j < limit {
+		j += 1
+
+		id := rand.New(seed).Uint64() // random number generation with validation when destination sends packet back
+
+		n := binary.PutUvarint(buffersendPacket, id) //encodes a uint64 into sendPacket and returns the number of bytes written and the func panic if buffer is too small
+		buffersendPacket[n] = 0
+
+		t1 := time.Now() //fetching the time sent for later calculations
+		_, err = udpConnection.Write(buffersendPacket)
+		check(err)
+
+		_, _, err = udpConnection.ReadFrom(bufferreceivePacket)
+		check(err)
+
+		ret_id, n := binary.Uvarint(bufferreceivePacket)
+		//decoding
+		if ret_id == id {
+			t2, _ := binary.Varint(bufferreceivePacket[n:])//time of receive
+			difference := (t2 - t1.UnixNano())  //unixnano refers to nanosecond range of time
+			ans += difference
+			i += 1 
+		}
+	}
+
+	//if i != count {
+	//	check(fmt.Errorf("Error, exceeded maximum number of attempts"))
+	//}
+
+	var difference2 float64 = float64(total) / float64(i)//final result
+
+	fmt.Printf("\nClient Address, IP and Port: %s\n", addressclient);
+	fmt.Printf("\nServer Address, IP and Port: %s\n",addressserver);
+	fmt.Println("Results Obtained as follows:")
+	// Result is printed in milliseconds, so divide by 1e6 from nano
+	fmt.Printf("\tRTT - %.3fms\n", difference2/1e6)
+	fmt.Printf("\tLatency - %.3fms\n", difference2/2e6)//since we take RTT as 2x Latency
+}
---
+++ Latency_Server_Timestamp.go
@@ -0,0 +1,73 @@
+
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/sciond"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func printUsage() {
+	fmt.Println("\ntimestamp_server -s ServerSCIONAddress")
+	fmt.Println("\tThe SCION address is specified as ISD-AS,[IP Address]:Port")
+	//fmt.Println("\tListens for incoming connections and responds back to them right away")
+	//fmt.Println("\tExample SCION address-s 19-ffaa:1:152,[192.168.0.102]:30102\n")
+}
+
+func main() {
+	var (
+		addressserver string
+
+		err    error
+		server *snet.Addr
+
+		udpConnection *snet.Conn
+	)
+
+
+  flag.StringVar(&addressserver, "s", "", "Server SCION Address")
+	flag.Parse()
+
+  //if len(serverAddress) > 0 {
+		//server, err = snet.AddrFromString(serverAddress)
+		//check(err)
+	//} else {
+		//printUsage()
+		//check(fmt.Errorf("Error, server address needs to be specified with -s"))
+	//}
+
+	for  len(addressserver)>0           //  statement refers to equatting  length of clientaddress with a condition
+	{
+		server, err = snet.AddrFromString(addressserver)
+		check(err)
+	}
+
+	dispatcherAddr := "/run/shm/dispatcher/default.sock"
+	snet.Init(server.IA, sciond.GetDefaultSCIONDPath(nil), dispatcherAddr)
+
+	udpConnection, err = snet.ListenSCION("udp4", server)
+	check(err)
+
+	bufferreceivePacket := make([]byte, 5000)//packet buffer array of size 5000 made for receiving
+	for {
+		n, addressclient, err := udpConnection.ReadFrom(bufferreceivePacket)
+		check(err)
+
+		// Packet received, send back response to same client
+		m := binary.PutVarint(bufferreceivePacket[n:], time.Now().UnixNano())//packet received from source and encodes a uint64 into receivePacket and returns the number of bytes written and the func panic if buffer is too small
+		_, err = udpConnection.WriteTo(bufferreceivePacket[: n+m], addressclient)
+		check(err)
+		fmt.Println("Received connection from", addressclient)
+	}
+}
