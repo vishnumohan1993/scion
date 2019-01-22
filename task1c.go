c37ed031b912186effd2a882a65b327dcb9ae775
vishnumohan1993 <38094703+vishnumohan1993@users.noreply.github.com>
2019-01-22 15:45:53 +0100

    Update task1c.go

--- task1c.go
+++ task1c.go
@@ -79,7 +79,7 @@ udpConnection,e2 = snet.DialSCION("udp4", local, remote)
 		// final result is plotted when returning id matches with original one
 		//if ret_id == id {
 			t2, _ := binary.Varint(bufferreceivePacket[n:])//time of receive
-			difference := (t2 - t1.UnixNano())  //unixnano refers to nanosecond range of time 
+			difference = (t2 - t1.UnixNano())  //unixnano refers to nanosecond range of time 
 		//}
 
 	//fmt.Printf("\nClient Address, IP and Port: %s\n",addressclient);
